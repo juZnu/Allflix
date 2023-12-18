5623d57f37be28302fc0af17400ac2535d9281f2
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2023-12-17 20:12:45 -0500

    Database linked

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'allflix',
+        'USER':'Jishnu',
+        'PASSWORD':'D0uble t@p',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
